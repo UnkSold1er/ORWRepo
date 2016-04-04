4ed373152600ce110aa723ffb17efb685ba32d85
UnkSold1er <adamnadara@gmail.com>
2016-04-04 13:44:30 +0200

    Lab11Blog

---
+++ Lab11Blog/blog-nadara-135-b/venv-nadara-135-b/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\python27\scripts\blog-nadara-135-b\venv-nadara-135-b\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
